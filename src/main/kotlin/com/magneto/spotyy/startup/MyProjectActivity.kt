4dad6db5813dd395b0a0f951ca3b4e6658e6468b
magneto3572 <bishal.singh.3572@gmail.com>
2026-04-11 11:35:37 +0530

    Add review nudge notification to 0.0.5
    Shows a one-time balloon after 3 days of use asking for a JetBrains
    Marketplace review. Registers Spotyy notification group in plugin.xml
    and wires ReviewNudgeService into MyProjectActivity.

--- src/main/kotlin/com/magneto/spotyy/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/magneto/spotyy/startup/MyProjectActivity.kt
@@ -1,9 +1,8 @@
 package com.magneto.spotyy.startup
 
-import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
-import com.intellij.openapi.application.ApplicationManager
+import com.magneto.spotyy.review.ReviewNudgeService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -11,8 +10,7 @@ class MyProjectActivity : ProjectActivity {
 
     override suspend fun execute(project: Project) {
         withContext(Dispatchers.IO) {
-            // Nothing to do here for now - removed sample code warning
-            // Any future initialization should happen here
+            ReviewNudgeService.maybeShowReviewNudge(project)
         }
     }
 }
